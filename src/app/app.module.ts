e3eda62c75d5326bf376fe0e8421ec1d825b8b07
Anubhav Vishwakarma <Anubhav.Vishwakarma@check24.de>
2021-01-18 20:06:48 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { MachineListComponent } from './machine-list/machine-list.component';
+import { StatusPipe } from './status.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MachineListComponent,
+    StatusPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
